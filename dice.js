593a15d96dfb6b2799ea3e6a1d3dc4428e7cb722
Nathan Robinson <Nathan11Robinson@gmail.com>
2021-02-04 09:37:45 +0000

    #4 Working reset button
    changes score to 0 when pressed

--- dice.js
+++ dice.js
@@ -1,3 +1,18 @@
 const buttonRoll = document.getElementById('roll');
 const buttonReset = document.getElementById('reset');
+let score = document.getElementById('score');
+let gameScore = 10;
 
+score.textContent = gameScore;
+
+//roll button - generates random number between 1 and 6 and stores score in score text
+
+
+
+
+//reset button - makes score change to 0 on click
+buttonReset.addEventListener('click', ()=>{
+
+    score.textContent = 0;
+
+});
